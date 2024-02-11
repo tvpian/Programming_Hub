a94c3a14b5a0cbc96a65d7ecd77bd0ad41f08bf4
Tharun Puthanveettil <tvpian@umd.edu>
2024-02-11 17:08:19 -0500

    Add pop() to the LinkedList class

--- python/linked_list/linked_list_c1.py
+++ python/linked_list/linked_list_c1.py
@@ -3,18 +3,21 @@ class Node:
         self.value = value
         self.next = None
 
+
 class LinkedList:
     def __init__(self, value):
         self.head = Node(value)
         self.tail = self.head
         self.length = 1
-    
+
     def print_list(self):
+        print("The following are the elements in the list:")
         temp = self.head
         while temp != None:
             print(temp.value)
             temp = temp.next
-            
+        print("------------------------------------------")
+
     def append(self, value):
         new_node = Node(value)
         if self.head == self.tail:
@@ -26,28 +29,64 @@ class LinkedList:
             temp.next = new_node
             self.tail = new_node
         self.length += 1
-          
+        print("{} is appended to the list".format(value))
+        print("Head is pointing to {}".format(self.head.value))
+        print("Tail is pointing to {}".format(self.tail.value))
+        print("------------------------------------------")
+
     def delete_node(self, value):
         if self.head == self.tail:
             self.head.next = None
             self.tail.next = None
             print("{} is deleted from the list".format(value))
+            print("Head is pointing to {}".format(self.head.value))
+            print("Tail is pointing to {}".format(self.tail.value))
+            print("------------------------------------------")
         elif self.head.value == value:
             temp = self.head
             self.head = self.head.next
             temp.next = None
             print("{} is deleted from the list".format(value))
+            print("Head is pointing to {}".format(self.head.value))
+            print("Tail is pointing to {}".format(self.tail.value))
+            print("------------------------------------------")
         else:
             temp = self.head
             while temp != self.tail:
                 if temp.next.value == value:
                     temp.next = temp.next.next
                     print("{} is deleted from the list".format(value))
+                    print("Head is pointing to {}".format(self.head.value))
+                    print("Tail is pointing to {}".format(self.tail.value))
+                    print("------------------------------------------")
                     break
                 temp = temp.next
-            
-            
-            
+
+    def pop_node(self):
+        # If the list is empty
+        if self.head == None:
+            print("No elements in the list to pop")
+        # If there is only element in the list
+        elif self.head == self.tail:
+            # Popping the last element from the list
+            print("Last element {} is popped from the list".format(self.tail.value))
+            self.head = None
+            self.tail = None
+            print("Head is pointing to None")
+            print("Tail is pointing to None")
+            print("------------------------------------------")
+        else:
+            temp = self.head
+            while temp.next.next != None:
+                temp = temp.next
+            print("Last element {} is popped from the list".format(self.tail.value))
+            self.tail = temp
+            self.tail.next = None
+            print("Head is pointing to {}".format(self.head.value))
+            print("Tail is pointing to {}".format(self.tail.value))
+            print("------------------------------------------")
+
+
 new_list = LinkedList(6)
 new_list.append(10)
 new_list.append(3)
@@ -55,4 +94,8 @@ new_list.append(5)
 new_list.print_list()
 new_list.delete_node(5)
 new_list.print_list()
-
+new_list.pop_node()
+new_list.pop_node()
+new_list.pop_node()
+new_list.pop_node()
+new_list.print_list()
